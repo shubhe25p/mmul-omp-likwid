7d3594b58406c7ff73d299b3d0acaa2499469b44
shubhe25p <shubhpachchigar@gmail.com>
2023-10-15 00:29:24 -0700

    basic-omp-likwid

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -2,7 +2,7 @@
 #include <omp.h>
 #include "likwid-stuff.h"
 
-const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
+const char *dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
 
 /*
  * This routine performs a dgemm operation
@@ -10,13 +10,28 @@ const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm
  * where A, B, and C are n-by-n matrices stored in column-major format.
  * On exit, A and B maintain their input values.
  */
-void square_dgemm(int n, double* A, double* B, double* C) 
+void square_dgemm(int n, double *A, double *B, double *C)
 {
-   // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
+// insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
 
-   // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
-   // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
-   // after the matrix multiply code but before the end of the parallel code block.
-
-   std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+// be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
+// but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
+// after the matrix multiply code but before the end of the parallel code block.
+#pragma omp parallel {
+   LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+#pragma omp for
+   for (int i = 0; i < n; i++)
+   {
+      for (int j = 0; j < n; j++)
+      {
+         double dot = 0.0;
+         for (int k = 0; k < n; k++)
+         {
+            dot += A[j + k * n] * B[i * n + k];
+         }
+         C[i * n + j] += dot;
+      }
+   }
+   LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+}
 }
