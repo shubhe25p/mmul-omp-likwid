8839fea8723544fd91b84196a676eb91a7d743d9
shubhe25p <shubhpachchigar@gmail.com>
2023-10-15 16:07:10 -0700

    blocked-omp

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -17,9 +17,10 @@ void square_dgemm(int n, double *A, double *B, double *C)
 // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
 // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
 // after the matrix multiply code but before the end of the parallel code block.
-#pragma omp parallel {
+#pragma omp parallel 
+{
    LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
-#pragma omp for
+   #pragma omp for
    for (int i = 0; i < n; i++)
    {
       for (int j = 0; j < n; j++)
--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -18,6 +18,47 @@ void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C
    // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
    // after the matrix multiply code but before the end of the parallel code block.
+   std::vector<double> buf(3 * block_size * block_size);
+   double *Ccopy = buf.data() + 0;
+   double *Bcopy = Ccopy + block_size * block_size;
+   double *Acopy = Bcopy + block_size * block_size;
 
-   std::cout << "Insert your blocked matrix multiply with copy optimization, openmp-parallel edition here " << std::endl;
+   for (int i0 = 0; i0 < n; i0 += block_size)
+   {
+      for (int j0 = 0; j0 < n; j0 += block_size)
+      {
+         for (int copyIndex = 0; copyIndex < block_size; copyIndex++)
+         {
+            memcpy(&Ccopy[copyIndex * block_size], &C[n * copyIndex + i0 * n + j0], sizeof(double) * block_size);
+         }
+         for (int k0 = 0; k0 < n; k0 += block_size)
+         {
+            for (int copyIndex = 0; copyIndex < block_size; copyIndex++)
+            {
+               memcpy(&Acopy[copyIndex * block_size], &A[n * copyIndex + k0 * n + j0], sizeof(double) * block_size);
+            }
+            for (int copyIndex = 0; copyIndex < block_size; copyIndex++)
+            {
+               memcpy(&Bcopy[copyIndex * block_size], &B[n * copyIndex + k0 + i0 * n], sizeof(double) * block_size);
+            }
+            #pragma omp parallel for
+            for (int i1 = 0; i1 < block_size; i1++)
+            {
+               for (int j1 = 0; j1 < block_size; j1++)
+               {
+                  double dot = 0.0;
+                  for (int k1 = 0; k1 < block_size; k1++)
+                  {
+                     dot += Acopy[k1 * block_size + j1] * Bcopy[i1 * block_size + k1];
+                  }
+                  Ccopy[i1 * block_size + j1] += dot;
+               }
+            }
+            for (int copyIndex = 0; copyIndex < block_size; copyIndex++)
+            {
+               memcpy(&C[n * copyIndex + i0 * n + j0], &Ccopy[copyIndex * block_size], sizeof(double) * block_size);
+            }
+         }
+      }
+   }
 }
